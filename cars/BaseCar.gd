09a0244fcdceb2642b9ace4fdbe657f5b2646e08
Manik Sharma <maniksharma2607@gmai.com>
2023-06-21 21:17:24 +0530

    basic stuff changes

--- cars/BaseCar.gd
+++ cars/BaseCar.gd
@@ -4,12 +4,14 @@ extends VehicleBody3D
 @export var STEER_SPEED = 1.5
 @export var STEER_LIMIT = 0.6
 var steer_target = 0
-@export var engine_force_value = 40
+@export var max_speed = 200
 @export var Blur_Amount=0.02
+@export var force_curve : Curve
 
-var stear_mode=1
+var stear_mode = 1
 func _physics_process(delta):
-	var speed=-$wheal3.get_rpm()*0.377*$wheal3.wheel_radius
+#	var speed=-$wheal3.get_rpm()*0.377*$wheal3.wheel_radius
+	var speed=linear_velocity.length()*3.6
 	traction(speed)
 #	$Hud/speed.text=str(round(speed))
 	
@@ -17,29 +19,23 @@ func _physics_process(delta):
 #	$"Speed lines".material.set("shader_parameter/mask_edge",remap(speed,30,200,1,0.4))
 	if Input.is_action_pressed("r"):
 		get_tree().reload_current_scene()
-	var fwd_mps = transform.basis.x.x
+		
+	var fwd_map = transform.basis.x.x
+		
 	steer_target = Input.get_action_strength("a") - Input.get_action_strength("d")
 	steer_target *= STEER_LIMIT
+	var speed_scale = remap(speed,0,max_speed,0,1)
 	if Input.is_action_pressed("s"):
-	# Increase engine force at low speeds to make the initial acceleration faster.
-
-		if speed < 20 and speed != 0:
-			engine_force = clamp(engine_force_value , 0, 300)
-		else:
-			engine_force = engine_force_value
+		engine_force = force_curve.sample(speed_scale)
 	else:
 		engine_force = 0
+		
 	if Input.is_action_pressed("w"):
-		# Increase engine force at low speeds to make the initial acceleration faster.
-		if fwd_mps >= -1:
-			if speed < 30 and speed != 0:
-				engine_force = -clamp(engine_force_value , 0, 300)
-			else:
-				engine_force = -engine_force_value
+		if fwd_map>=-2:
+			engine_force = -force_curve.sample(speed_scale)
 		else:
-			brake = 1
-	else:
-		brake = 0.0
+			brake=0
+
 		
 	if Input.is_action_pressed("break"):
 		brake=3
@@ -48,12 +44,11 @@ func _physics_process(delta):
 	else:
 		$wheal2.wheel_friction_slip=3
 		$wheal3.wheel_friction_slip=3
+		brake=0
 	if stear_mode:
 		steering = $Hud/Control/stearing.steer_amount
 	else:
 		steering = move_toward(steering, steer_target, STEER_SPEED * delta)
-
-
 
 
 
@@ -68,14 +63,5 @@ func traction(speed):
 
 
 
-func _on_h_slider_value_changed(value):
-	STEER_SPEED=value
 
-
-func _on_h_slider_value_changed_speed(value):
-	engine_force_value=value
-
-
-func _on_blur_value_changed(value):
-	Blur_Amount=value
 
--- cars/stearing.gd
+++ cars/stearing.gd
@@ -1,21 +1,28 @@
 extends Sprite2D
 
-@export var stear_limit:float = 540
+@export var stear_limit:float = 360
 var steer_amount=0.0
 var turning=false
 var rot:=0.0
 var start_vec=Vector2.ZERO
-
+var index : int = -1
 func _input(event):
-	if event is InputEventScreenDrag:
-		drag(event)
-	if event is InputEventScreenTouch:
+	if event is InputEventScreenDrag :
+		if index == event.index:
+			drag(event)
+		
+			
+	if event is InputEventScreenTouch :
 		var touch = event as InputEventScreenTouch
 		if touch.pressed:
 			rot = Vector2(event.position-global_position).angle_to(start_vec)
 			start_vec=event.position - global_position
+			if is_in_circle(event.position,350) and index==-1:
+				index = event.index
 		else:
-			turning=false
+			if index==event.index:
+				turning=false
+				index = -1
 
 func _process(delta: float) -> void:
 	if not turning:
@@ -30,7 +37,13 @@ func drag(event):
 	rotation=clamp(rotation-rot,deg_to_rad(-stear_limit),deg_to_rad(stear_limit))
 	
 	
-	
+func is_in_circle(pos:Vector2,radi:float) -> bool:
+#	print(pos,pos.distance_to(position))
+	if pos.distance_to(global_position) < radi:
+		return true
+	else:
+		return false
+		
 
 
 
