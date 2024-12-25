4ccff9bdfa4173e6b77811ed6ed83656984e133c
= <=>
2024-12-25 12:45:55 +0530

    mobile changes

--- UI/Hud.gd
+++ UI/Hud.gd
@@ -6,8 +6,7 @@ func _ready():
 #	$"Panel/VBoxContainer/EnginForce/Control/HSlider".value = get_parent().engine_force_value
 #	$Panel/VBoxContainer/Stearspeed/Control/HSlider.value = get_parent().STEER_SPEED
 
-func _on_button_pressed():
-	$Panel.show()
 
-func _on_button_button_down():
-	$Panel.hide()
+
+func _on_settings_toggled(toggled_on):
+	$Panel.visible = !$Panel.visible
